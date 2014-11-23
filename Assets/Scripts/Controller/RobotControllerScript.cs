a185b68b912c7d130b0cb81a64bc579a912f8a1c
VanGoghsCoffee <mail@davidrochholz.de>
2014-11-23 17:30:17 +0100

    Character added

---
+++ Assets/Scripts/Controller/RobotControllerScript.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotControllerScript : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+
+    bool facingRight = true;
+    
+    // ground
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public float jumpForce = 300;
+
+    bool doubleJump = false;
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+
+        if (grounded)
+            doubleJump = false;
+
+        anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
+        
+        float move = Input.GetAxis("Horizontal");
+
+		anim.SetFloat ("Speed", Mathf.Abs(move));
+
+        rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+	}
+
+    void Update()
+    {
+        if ((grounded || !doubleJump) && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Ground", false);
+            rigidbody2D.AddForce(new Vector2(0, jumpForce));
+
+            if (!doubleJump && !grounded)
+                doubleJump = true;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Assets/Scripts/World/EndlessBG.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndlessBG : MonoBehaviour {
+
+    private Vector3 m_BackPos;
+    private float m_sMoveHeight = 20f;
+    private bool waited;
+
+    IEnumerator Wait()
+    {
+        print(Time.time);
+        waited = false;
+        yield return new WaitForSeconds(5f);
+        waited = true;
+        print(Time.time);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        while (waited)
+        { }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnBecameInvisible()
+    {
+        m_sMoveHeight = gameObject.renderer.bounds.size.y - 0.03f;
+        // get current position
+        m_BackPos = gameObject.transform.position;
+
+        // move to new position when invisible
+        gameObject.transform.position = new Vector3(m_BackPos.x, m_BackPos.y + 3 * m_sMoveHeight);
+    }
+}

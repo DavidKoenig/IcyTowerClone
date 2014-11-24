68d5fa9fb365a9d4b16d486e55e3f1af7505f669
DavidKoenig <david.koenig@fh-erfurt.de>
2014-11-24 18:49:03 +0100

    added score and highscore again

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -4,16 +4,22 @@ using System.Collections;
 public class GameController : MonoBehaviour {
 
     private static int m_sScore;
+    private GUIText m_GUIScore;
+    private GUIText m_GUIHighscore;
 	
     // Use this for initialization
 	void Start () {
         m_sScore = 0;
         Physics2D.IgnoreLayerCollision(13, 12, true);
+        m_GUIScore = GameObject.Find("Score").GetComponent<GUIText>() as GUIText;
+        m_GUIHighscore = GameObject.Find("Highscore").GetComponent<GUIText>() as GUIText;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () 
     {	    
+        m_GUIScore.text     = "Score: " + m_sScore.ToString();
+        m_GUIHighscore.text = "Highscore: " + m_sScore.ToString();
 	}
 
     void OnGUI()
@@ -26,10 +32,10 @@ public class GameController : MonoBehaviour {
         //GUI.skin.label.fontSize = 25;
         //GUI.Label(new Rect(Screen.width - 1170, 610, 200, 50), "Highscore: " + m_sScore.ToString());
 
-        GUI.color = Color.white;
-        GUILayout.Label("Score: " + m_sScore.ToString());
-        GUI.color = Color.white;
-        GUILayout.Label("Highscore: " + m_sScore.ToString());
+        //GUI.color = Color.white;
+        //GUILayout.Label("Score: " + m_sScore.ToString());
+        //GUI.color = Color.white;
+        //GUILayout.Label("Highscore: " + m_sScore.ToString());
     }
 
     public static void IncreaseScore ()

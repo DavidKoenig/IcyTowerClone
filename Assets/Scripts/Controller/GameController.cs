e0c9956a17cc7829dfdfad4a00e33942ad1d4ce8
VanGoghsCoffee <mail@davidrochholz.de>
2014-11-25 09:12:11 +0100

    Player saved

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -11,16 +11,16 @@ public class GameController : MonoBehaviour {
 	
     // Use this for initialization
 	void Start () {
+        Physics2D.IgnoreLayerCollision(13, 12, true);
+
         m_sScore     = 0;
         m_sHighscore = 0;
-
+       
         m_GUIScore      = GameObject.Find("Score").GetComponent<GUIText>() as GUIText;
         m_GUIHighscore  = GameObject.Find("Highscore").GetComponent<GUIText>() as GUIText;
         m_GUIHurry      = GameObject.Find("Hurry").GetComponent<GUIText>() as GUIText;
-
+       
         m_GUIHurry.color = new Color(m_GUIHurry.color.r, m_GUIHurry.color.g, m_GUIHurry.color.b, 0f);
-
-        Physics2D.IgnoreLayerCollision(13, 12, true);
 	}
 	
 	// Update is called once per frame
@@ -32,18 +32,18 @@ public class GameController : MonoBehaviour {
 
     void OnGUI()
     {
-        //GUI.color = Color.black;
-        //GUI.skin.label.fontSize = 25;
-        //GUI.Label(new Rect(Screen.width - 390, 610, 200, 50), "Score: " + m_sScore.ToString());
+        GUI.color = Color.black;
+        GUI.skin.label.fontSize = 25;
+        GUI.Label(new Rect(Screen.width - 390, 610, 200, 50), "Score: " + m_sScore.ToString());
 
-        //GUI.color = Color.black;
-        //GUI.skin.label.fontSize = 25;
-        //GUI.Label(new Rect(Screen.width - 1170, 610, 200, 50), "Highscore: " + m_sScore.ToString());
+        GUI.color = Color.black;
+        GUI.skin.label.fontSize = 25;
+        GUI.Label(new Rect(Screen.width - 1170, 610, 200, 50), "Highscore: " + m_sScore.ToString());
 
-        //GUI.color = Color.white;
-        //GUILayout.Label("Score: " + m_sScore.ToString());
-        //GUI.color = Color.white;
-        //GUILayout.Label("Highscore: " + m_sScore.ToString());
+        GUI.color = Color.white;
+        GUILayout.Label("Score: " + m_sScore.ToString());
+        GUI.color = Color.white;
+        GUILayout.Label("Highscore: " + m_sScore.ToString());
     }
 
     public void IncreaseScore ()
@@ -59,12 +59,12 @@ public class GameController : MonoBehaviour {
 
     IEnumerator FadingIn()
     {
-        yield return new WaitForSeconds(0.1f);
-        while (true)
-        {
-            m_GUIHurry.color = new Color(m_GUIHurry.color.r, m_GUIHurry.color.g, m_GUIHurry.color.b, 1f);
-            yield return new WaitForSeconds(0.1f);
-        }
+       yield return new WaitForSeconds(0.1f);
+       while (true)
+       {
+           m_GUIHurry.color = new Color(m_GUIHurry.color.r, m_GUIHurry.color.g, m_GUIHurry.color.b, 1f);
+           yield return new WaitForSeconds(0.1f);
+       }
     }
 
     private void FadingOut()

7629cb64d5feeacc951bdfc634571cc735fcf32f
DavidKoenig <david.koenig@fh-erfurt.de>
2014-11-25 09:30:03 +0100

    Animation of Hurry Text; some fixes

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -10,15 +10,14 @@ public class GameController : MonoBehaviour {
     private GUIText m_GUIHurry;
 	
     // Use this for initialization
-	void Start () {
+	void Start () 
+    {
         m_sScore     = 0;
         m_sHighscore = 0;
 
         m_GUIScore      = GameObject.Find("Score").GetComponent<GUIText>() as GUIText;
         m_GUIHighscore  = GameObject.Find("Highscore").GetComponent<GUIText>() as GUIText;
         m_GUIHurry      = GameObject.Find("Hurry").GetComponent<GUIText>() as GUIText;
-
-        m_GUIHurry.color = new Color(m_GUIHurry.color.r, m_GUIHurry.color.g, m_GUIHurry.color.b, 0f);
 
         Physics2D.IgnoreLayerCollision(13, 12, true);
 	}
@@ -32,18 +31,6 @@ public class GameController : MonoBehaviour {
 
     void OnGUI()
     {
-        //GUI.color = Color.black;
-        //GUI.skin.label.fontSize = 25;
-        //GUI.Label(new Rect(Screen.width - 390, 610, 200, 50), "Score: " + m_sScore.ToString());
-
-        //GUI.color = Color.black;
-        //GUI.skin.label.fontSize = 25;
-        //GUI.Label(new Rect(Screen.width - 1170, 610, 200, 50), "Highscore: " + m_sScore.ToString());
-
-        //GUI.color = Color.white;
-        //GUILayout.Label("Score: " + m_sScore.ToString());
-        //GUI.color = Color.white;
-        //GUILayout.Label("Highscore: " + m_sScore.ToString());
     }
 
     public void IncreaseScore ()
@@ -51,28 +38,9 @@ public class GameController : MonoBehaviour {
         m_sScore += 10;
         if (m_sScore >= 50 && (m_sScore % 50) == 0)
         {
-            StartCoroutine(FadingIn());
-            m_GUIHurry.color = new Color(m_GUIHurry.color.r, m_GUIHurry.color.g, m_GUIHurry.color.b, 0f);
-            CameraMovement.IncreaseCameraSpeed();       
-        }
-    }
+            m_GUIHurry.animation.Play("HurryText");
+            CameraMovement.IncreaseCameraSpeed();
 
-    IEnumerator FadingIn()
-    {
-        yield return new WaitForSeconds(0.1f);
-        while (true)
-        {
-            m_GUIHurry.color = new Color(m_GUIHurry.color.r, m_GUIHurry.color.g, m_GUIHurry.color.b, 1f);
-            yield return new WaitForSeconds(0.1f);
         }
-    }
-
-    private void FadingOut()
-    {
-        for (float a = 1f; a >= 0f; a -= 0.1f)
-        {
-            m_GUIHurry.color = new Color(m_GUIHurry.color.r, m_GUIHurry.color.g, m_GUIHurry.color.b, a);
-        }
-        m_GUIHurry.color = new Color(m_GUIHurry.color.r, m_GUIHurry.color.g, m_GUIHurry.color.b, 0f);
     }
 }
